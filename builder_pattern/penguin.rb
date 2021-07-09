c5d827daf55215435ad9b9c562f50f24137e793e
Nabeel Ahsen <nabeel.ahsen@shopify.com>
2021-07-09 10:53:58 -0400

    Extract PenguinBuilder from Penguin

--- builder_pattern/penguin.rb
+++ builder_pattern/penguin.rb
@@ -1,10 +1,26 @@
+class PenguinBuilder
+  attr_reader :penguin
+
+  def self.build
+    builder = new
+    yield(builder)
+    builder.penguin
+  end
+
+  def initialize
+    @penguin = Penguin.new
+  end
+
+  def set_beak_colour(colour)
+    @penguin.beak_colour = colour
+  end
+
+  def set_height(height)
+    @penguin.height = height
+  end
+end
+
 class Penguin
   attr_accessor :beak_colour
   attr_accessor :height
-
-  def self.build
-    penguin = new
-    yield(penguin)
-    penguin
-  end
 end
--- builder_pattern/penguin_test.rb
+++ builder_pattern/penguin_test.rb
@@ -2,17 +2,25 @@ require "test/unit"
 require_relative "penguin"
 
 class PenguinTest < Test::Unit::TestCase
-  def test_build_penguin_with_beak
-    penguin = Penguin.build do |penguin|
-      penguin.beak_colour = "orange"
+  def test_build_penguin_specifying_beak_colour
+    penguin = PenguinBuilder.build do |builder|
+      builder.set_beak_colour("orange")
     end
 
     assert_equal penguin.beak_colour, "orange"
   end
 
+  def test_build_beak_colour_can_only_be_string
+    assert_raises do
+      PenguinBuilder.build do |builder|
+        builder.set_beak_colour(true)
+      end
+    end
+  end
+
   def test_build_penguin_given_height
-    penguin = Penguin.build do |penguin|
-      penguin.height = 5.8
+    penguin = PenguinBuilder.build do |builder|
+      builder.set_height(5.8)
     end
 
     assert_equal penguin.height, 5.8

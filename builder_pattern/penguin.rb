1ca9cd02642ef6b618b6c11e38c39980d0c6b6aa
Nabeel Ahsen <nabeel.ahsen@shopify.com>
2021-07-09 11:06:48 -0400

    Validate input when building a Penguin

--- builder_pattern/penguin.rb
+++ builder_pattern/penguin.rb
@@ -1,22 +1,43 @@
 class PenguinBuilder
-  attr_reader :penguin
-
   def self.build
     builder = new
     yield(builder)
     builder.penguin
   end
 
+  attr_reader :penguin
+
   def initialize
     @penguin = Penguin.new
   end
 
   def set_beak_colour(colour)
-    @penguin.beak_colour = colour
+    @penguin.beak_colour = validate_beak_colour(colour)
   end
 
   def set_height(height)
-    @penguin.height = height
+    @penguin.height = validate_height(height)
+  end
+
+  private
+
+  VALID_BEAK_COLOURS = [
+    "orange",
+    "red",
+    "green",
+    "yellow"
+  ]
+
+  def validate_beak_colour(colour)
+    raise StandardError unless VALID_BEAK_COLOURS.include?(colour)
+
+    colour
+  end
+
+  def validate_height(height)
+    raise StandardError unless height.kind_of? Float
+
+    height
   end
 end
 
--- builder_pattern/penguin_test.rb
+++ builder_pattern/penguin_test.rb
@@ -18,11 +18,35 @@ class PenguinTest < Test::Unit::TestCase
     end
   end
 
+  def test_build_beak_colour_must_be_a_valid_beak_colour
+    assert_raises do
+      PenguinBuilder.build do |builder|
+        builder.set_beak_colour("blue")
+      end
+    end
+  end
+
   def test_build_penguin_given_height
     penguin = PenguinBuilder.build do |builder|
       builder.set_height(5.8)
     end
 
     assert_equal penguin.height, 5.8
+  end
+
+  def test_build_penguin_height_must_be_not_be_integer
+    assert_raises do
+      PenguinBuilder.build do |builder|
+        builder.set_height(5)
+      end
+    end
+  end
+
+  def test_build_penguin_height_must_be_a_float
+    assert_raises do
+      PenguinBuilder.build do |builder|
+        builder.set_height("5.8")
+      end
+    end
   end
 end
